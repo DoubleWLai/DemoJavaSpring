611077d81b25e3a820597af968712ed2a153b10d
P <doublewlai@gmail.com>
2022-02-06 22:46:23 +0200

    add service

--- src/main/java/com/example/demo/dao/StudentDao.java
+++ src/main/java/com/example/demo/dao/StudentDao.java
@@ -1,5 +1,14 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Student;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface StudentDao {
+
+    List<Student> selectAllStudents();
+
+    int insertStudent(UUID id, Student student);
 
 }
---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.StudentDao;
+import com.example.demo.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class StudentService {
+
+    private StudentDao studentDao;
+
+    @Autowired
+    public StudentService(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentDao.selectAllStudents();
+    }
+}
